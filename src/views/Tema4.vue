6b290fa83f062efa378149fc51093cc824f7ca73
Lucenith Pinilla Moreno <167031042+LucenithGitHub@users.noreply.github.com>
2025-03-11 20:02:27 -0500

    Avance Maquetación 2 03.11

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -5,7 +5,173 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 4
-      h1 Tema 4
+      h1 Concepto de recibido y despacho
+
+    .row.justify-content-center.align-items-center.mb-4
+      p El recibo y despacho de mercancías son procesos fundamentales en las empresas, cuyo objetivo es garantizar la correcta entrada y salida de productos, siguiendo una serie de documentos y requisitos establecidos por los clientes. Estas operaciones requieren de habilidades específicas por parte del personal encargado, dado que tienen una alta responsabilidad sobre el control de los movimientos de inventario
+
+    .row.justify-content-center.align-items-center.mb-4
+      .col-lg-9.col-sm-12.col-md-12
+        .row.mb-2
+          .col-12.col-md-8.col-lg-5.p-0.bgprimario
+            img(src='@/assets/curso/temas/t4/img1.png', alt='')
+          .col-12.col-md-12.col-lg-7.bgsecundario
+            h4.mx-5.mt-4.mb-2.bgacentocontenido.p-2 Concepto de Recibo:
+            p.mx-5.mb-2 el recibo de mercancías implica una serie de operaciones que incluyen la recepción, descarga, verificación y almacenamiento de productos, así como la gestión de los documentos relacionados con dicha recepción. Este proceso representa el primer paso en la gestión de un almacén e involucra materias primas, productos en proceso y productos terminados. La operación concluye cuando el producto es correctamente ubicado en las áreas designadas.
+        p.mt-4 Las actividades de recibido y despacho se llevan a cabo de manera continua en los almacenes, bodegas y centros de distribución, los cuales requieren una coordinación eficiente entre proveedores y clientes para asegurar un flujo constante de información.
+      .col-lg-3.col-sm-6.col-md-6
+        img(src='@/assets/curso/temas/t4/img2.png', alt='')
+
+    .row.justify-content-center.align-items-center.mb-4
+      .col-lg-5.col-sm-8.col-md-8
+        img(src='@/assets/curso/temas/t4/img3.png', alt='')
+      .col-lg-7.col-sm-12.col-md-12
+        p Las tareas relacionadas con el recibido de mercancías son de vital importancia, ya que requieren un manejo cuidadoso y protección de los productos. Estas actividades son fundamentales para el desarrollo y gestión de la organización, así como para satisfacer las demandas de los clientes durante los procesos de distribución. Las mercancías reciben un tratamiento interno que incluye almacenamiento, control de materias primas e insumos para la producción, gestión de inventarios, alistamiento, manipulación, acondicionamiento, entre otros.
+        .cajon.color-acento-contenido.p-4.mb-4
+          p El proceso de recibido y despacho comienza con la emisión de una orden de compra, la cual es una solicitud dirigida al proveedor en la que se especifican características, cantidades, términos de pago y condiciones de entrega.
+    p.mb-4 Existen diferentes tipos de almacenes, clasificados según la naturaleza de los productos almacenados, tales como almacenes de materias primas, productos semielaborados, productos terminados, piezas de recambio, materiales auxiliares y archivos de información. Los métodos de almacenamiento de mercancías varían según las necesidades, y algunas de las técnicas más utilizadas son:
+
+    .row.bggris.align-items-center.p-2.mb-0(data-aos="fade-down")
+      .col-lg-12.col-md-12.col-12.px-lg-5.px-4
+        .row.justify-content-center
+    
+        .row.justify-content-center.align-items-center.mb-5
+          .col-lg-8.col-md-10
+            SlyderF.custom(columnas="col-lg-6 col-md-6 col-xl-6").mt-4
+              .tarjeta.tarjeta--slyder.b1.p-2.p-lg-4.p-md-3
+                .row.justify-content-center.mb-3
+                  .col-8.my-2
+                    img.img-a.img-t(data-aos="zoom-in-left")(src='@/assets/curso/temas/t4/tar1-1.png' alt='AvatarTop')
+                .bgprimario.p-2.mb-4
+                  h4.text-center.mb-0 Convencionales
+                p.mb-0.text-center(data-aos="fade-right") Sistema tradicional de almacenamiento que emplea estanterías con acceso manual mediante carretillas.     
+              .tarjeta.tarjeta--slyder.b1.p-2.p-lg-4.p-md-3
+                .row.justify-content-center.mb-3
+                  .col-8.my-2
+                    img.img-a.img-t(data-aos="zoom-in-left")(src='@/assets/curso/temas/t4/tar1-2.png' alt='AvatarTop')
+                .bgprimario.p-2.mb-4
+                  h4.text-center.mb-0 #[em Drive-in]
+                p.mb-0.text-center(data-aos="fade-right") Sistema de almacenamiento que se caracteriza por la falta de espacios entre pasillos dentro de las estanterías, lo que permite una mayor densidad de almacenamiento.    
+              .tarjeta.tarjeta--slyder.b1.p-2.p-lg-4.p-md-3
+                .row.justify-content-center.mb-3
+                  .col-8.my-2
+                    img.img-a.img-t(data-aos="zoom-in-left")(src='@/assets/curso/temas/t4/tar1-3.png' alt='AvatarTop')
+                .bgprimario.p-2.mb-4
+                  h4.text-center.mb-0 Dinámicos
+                p.mb-0.text-center(data-aos="fade-right") Sistema de almacenamiento móvil, formado por bloques compactos sin pasillos. La principal característica es el deslizamiento de los pallets desde el punto de entrada hasta el de salida de la estantería.  
+              .tarjeta.tarjeta--slyder.b1.p-2.p-lg-4.p-md-3
+                .row.justify-content-center.mb-3
+                  .col-8.my-2
+                    img.img-a.img-t(data-aos="zoom-in-left")(src='@/assets/curso/temas/t4/tar1-4.png' alt='AvatarTop')
+                .bgprimario.p-2.mb-4
+                  h4.text-center.mb-0 Móviles
+                p.mb-0.text-center(data-aos="fade-right") Sistema en el que toda la estructura de estanterías se mueve, permitiendo abrir un pasillo entre cualquier estantería, mientras el resto se mantiene compacto.
+              .tarjeta.tarjeta--slyder.b1.p-2.p-lg-4.p-md-3
+                .row.justify-content-center.mb-3
+                  .col-8.my-2
+                    img.img-a.img-t(data-aos="zoom-in-left")(src='@/assets/curso/temas/t4/tar1-5.png' alt='AvatarTop')
+                .bgprimario.p-2.mb-4
+                  h4.text-center.mb-0 Semiautomáticos y automáticos
+                p.mb-0.text-center(data-aos="fade-right") Sistemas que automatizan el movimiento de los productos dentro de las zonas de almacenamiento, facilitando el acceso a cualquier artículo almacenado desde un punto de control centralizado.
+              .tarjeta.tarjeta--slyder.b1.p-2.p-lg-4.p-md-3
+                .row.justify-content-center.mb-3
+                  .col-8.my-2
+                    img.img-a.img-t(data-aos="zoom-in-left")(src='@/assets/curso/temas/t4/tar1-6.png' alt='AvatarTop')
+                .bgprimario.p-2.mb-4
+                  h4.text-center.mb-0 Autoportantes
+                p.mb-0.text-center(data-aos="fade-right") Este tipo de almacén utiliza estanterías que cumplen una doble función: almacenar productos y actuar como soporte estructural del edificio.
+          .col-lg-4.col-md-7.col-9
+            img(src='@/assets/curso/temas/t4/img6.png', alt='')
+    p.mt-4 Estas técnicas permiten optimizar el espacio y la eficiencia en el manejo de inventarios, adaptándose a las necesidades específicas de cada tipo de mercancía almacenada.
+
+    Separador  
+    #t_4_1.titulo-segundo.color-acento-contenido(data-aos="zoom-in-left")
+      h2 4.1 Los principales objetivos del proceso de recibido de mercancías
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-6.col-md-6.col-lg-3
+        img(src='@/assets/curso/temas/t4/img4.png', alt='')
+      .col-sm-12.col-md-12.col-lg-9
+        AcordionA(tipo="a" clase-tarjeta="tarjeta tarjeta--azul")
+          .row.col-xl-11.m-auto(titulo="Optimizar el tiempo de recepción")
+            .row.justify-content-center.align-items-center(data-aos="fade-right")
+              .col-lg-12
+                p.mb-0 Esto se logra mediante estándares específicos relacionados con las unidades recibidas por persona, el tipo de producto y el material de empaque, entre otros.
+          .row.col-xl-11.m-auto(titulo="Garantizar el cumplimiento de las especificaciones de calidad")
+            .row.justify-content-center.align-items-center(data-aos="fade-right")
+              .col-lg-12
+                p.mb-0 Asegurando que cada producto recibido cumpla con los requisitos establecidos previamente.
+          .row.col-xl-11.m-auto(titulo="Identificar inconformidades de manera temprana")
+            .row.justify-content-center.align-items-center(data-aos="fade-right")
+              .col-lg-12
+                p.mb-0 Detectar posibles problemas antes de que la mercancía sea almacenada, lo que ayuda a prevenir errores y mejora la eficiencia operativa.
+
+    p.mb-4 En el centro de distribución se encuentran diferentes áreas, las cuales se detallan a continuación:
+
+    .row.bg4.mb-4
+      .row.justify-content-center.align-items-center
+          .col-lg-10.col-md-12.col-sm-12
+            ImagenInfograficaB.color-acento-botones
+              template(v-slot:imagen)
+                figure
+                  img(src='@/assets/curso/temas/t4/pc1.png', alt='Texto que describa la imagen')
+  
+              div(x="7%" y="97%" tooltip="Ver más" numero="+")
+                .tarjeta.bgsecundario.p-4
+                  .row.align-items-center.justify-content-center
+                      p Los espacios destinados a las maniobras de los vehículos para entrar, salir y posicionarse para su descarga deben ser adecuados y funcionales. El número de muelles se determina considerando factores como el costo de los camiones en espera, las operaciones de descarga, el personal involucrado y el impacto en la programación de las descargas.
+
+              div(x="60%" y="95%" tooltip="Ver más" numero="+")
+                .tarjeta.bgsecundario.p-4
+                  .row.align-items-center.justify-content-center
+                    p Tras la descarga, es necesario proceder con la comprobación, verificando las cantidades y la clasificación antes de su almacenamiento.
+
+              div(x="33%" y="21%" tooltip="Ver más" numero="+")
+                .tarjeta.bgsecundario.p-4
+                  .row.align-items-center.justify-content-center
+                    p Zona destinada para almacenar mercancías durante un período determinado. Los espacios no dependen de la ubicación específica para su almacenamiento, sino de los pasillos y elementos estructurales (como estanterías de soporte), así como de los equipos de manipulación.
+
+              div(x="4%" y="56%" tooltip="Ver más" numero="+")
+                .tarjeta.bgsecundario.p-4
+                  .row.align-items-center.justify-content-center
+                    p Antes de proceder con la carga, la consolidación de las mercancías y la verificación final, se llevan a cabo tareas de paletización y estabilización mediante métodos de enfajado, utilizando film estirable, termorretráctil u otros materiales adecuados.
+
+              div(x="70%" y="58%" tooltip="Ver más" numero="+")
+                .tarjeta.bgsecundario.p-4
+                  .row.align-items-center.justify-content-center
+                    p Cámaras frigoríficas. Devoluciones.
+
+              div(x="35%" y="68%" tooltip="Ver más" numero="+")
+                .tarjeta.bgsecundario.p-4
+                  .row.align-items-center.justify-content-center
+                    p Espacios destinados para el almacenamiento, organización de las operaciones, oficinas y servicios auxiliares, tanto para el personal administrativo como operativo.
+
+              div(x="57%" y="31%" tooltip="Ver más" numero="+")
+                .tarjeta.bgsecundario.p-4
+                  .row.align-items-center.justify-content-center
+                    .col-6.col-md-6.col-lg-6
+                      .titulo-sexto.color-acento-contenido
+                        h5 Figura 1. 
+                        | Zona de #[em picking] y preparación
+                      figure
+                        img(src='@/assets/curso/temas/t4/img5.png', alt='La imagen representa una zona de picking y preparación de pedidos en un entorno logístico. Varias personas están organizando productos en cajas abiertas. La infraestructura del lugar incluye un sistema de transporte y almacenamiento optimizado para agilizar la distribución de mercancías.')
+                    .col-12.col-md-12.col-lg-6
+                      p Existen tres métodos para la recuperación de productos:
+                      ul.lista-ul--color.fa-al
+                        li 
+                          i.fas.fa-shopping-cart
+                          span.mb-0 #[b Selección individual:] consiste en recoger un solo producto desde una ubicación específica.
+                        li 
+                          i.fas.fa-shopping-cart
+                          span.mb-0 #[b Ruta de recogida:] en este caso, se recuperan varios productos diferentes de un mismo pedido antes de regresar a la zona de preparación.
+                        li 
+                          i.fas.fa-shopping-cart
+                          span.mb-0 #[b Asignación de zonas de recogida:] a cada trabajador se le asigna una zona específica para la recogida de productos.
+                  .row
+                    .col-lg-6.col-md-12.col-sm-12
+                      p Recuperación de productos desde sus lugares de almacenamiento y su preparación para su envío adecuado, según el nivel de automatización.
+
+    p Los centros de distribución están equipados con muelles de carga, los cuales dependen de su disposición y número. La correcta programación de la recepción de mercancías se ve influenciada por el conocimiento (o desconocimiento) de los tiempos de llegada de los vehículos. Se recomienda contar con muelles destinados tanto para la entrada como para la salida de mercancías.
 
 
 </template>
--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -5,7 +5,112 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 5
-      h1 Tema 5
+      h1 Métodos y técnicas de extracción de objetos
+
+    .row.align-items-center.justify-content-center.mb-4
+      .col-sm-10.col-md-10.col-lg-5
+        img(src='@/assets/curso/temas/t5/img1.png', alt='')
+      .col-sm-12.col-md-12.col-lg-7
+        .row.bggris.px-5.py-3.mb-4
+          ul.lista-ul--color.fa-al
+            li 
+              i.fas.fa-shopping-cart
+              span.mb-0 #[b Manual:] cargue y descargue, carretillas, gatos hidráulicos.
+            li 
+              i.fas.fa-shopping-cart
+              span.mb-0 #[b Mecanizado:] utilización de montacargas, bandas transportadoras.
+            li 
+              i.fas.fa-shopping-cart
+              span.mb-0 #[b Automatizado:] sistemas neumáticos.
+        .row.align-items-center.justify-content-center
+          .col-6.col-md-2.col-lg-2
+            img(src='@/assets/curso/temas/t5/ico1.png', alt='')
+          .col-sm-10.col-md-10.col-lg-10
+            p En los centros de distribución, la preparación de los pedidos implica la planificación y coordinación de diversas actividades, como alistamiento, acondicionamiento, cargue y descargue, entre otras.
+
+    .row.justify-content-center.align-items-center.mb-4
+      .col-lg-12
+        .bloque-texto-g.bloque-texto-g--inverso.color-acento-contenido.p-3.p-sm-4.p-md-5
+          .bloque-texto-g__img(
+            :style="{'background-image': `url(${require('@/assets/curso/temas/t5/img2.png')})`}"
+          )
+          .bloque-texto-g__texto.p-4
+            p El sistema de recogida debe estar diseñado para cumplir con los objetivos del centro de distribución, lo cual incluye garantizar una respuesta eficiente al cliente y el control de costos. Esto requiere optimización del tiempo, gestión efectiva del personal y los equipos, así como un manejo adecuado de la información. La preparación de los pedidos comienza con la clasificación y agrupación de los pedidos recibidos de los clientes, y con la determinación de cómo deben ser preparados para su despacho.
+            br
+            br
+            p En cuanto a la extracción de mercancías, se pueden adoptar dos enfoques: permitir que el operario se desplace hacia los productos, o bien, transportar los productos hacia los operarios, quienes permanecerán en un lugar fijo.
+
+    p.mb-4 Dentro del almacén, se diseñan actividades secuenciales y lógicas para responder eficazmente a los pedidos de los clientes. La extracción de los productos, conforme a las solicitudes de los clientes, se organiza mediante rutas de reparto, zonas geográficas y empresas de transporte.
+
+    .row.align-items-center.justify-content-center.mb-4
+      .col-sm-12.col-md-12.col-lg-5
+        .row.bgsecundario.px-5.py-4.mb-4
+          h4.bgacentocontenido.p-2.mt-4 La preparación de pedidos incluye las siguientes operaciones:
+          ul.lista-ul--color.fa-al.mt-2
+            li.mb-0
+              i.fas.fa-shopping-cart
+              span.mb-0 Recopilación de pedidos (captura de datos).
+            li.mb-0 
+              i.fas.fa-shopping-cart
+              span.mb-0 Gestión de los pedidos (sistema de preparación).
+            li.mb-0
+              i.fas.fa-shopping-cart
+              span.mb-0 Elaboración de los documentos de preparación.
+            li.mb-0 
+              i.fas.fa-shopping-cart
+              span.mb-0 Extracción (#[em picking]).
+            li.mb-0
+              i.fas.fa-shopping-cart
+              span.mb-0 Traslado a la zona de expedición.
+            li 
+              i.fas.fa-shopping-cart
+              span.mb-0 Verificación y acondicionamiento de los pedidos.
+      .col-3.col-md-6.col-lg-3
+        img(src='@/assets/curso/temas/t5/img3.png', alt='')
+      .col-sm-12.col-md-12.col-lg-4
+        .row
+          .col-6.col-md-3.col-lg-3
+            img(src='@/assets/curso/temas/t5/ico2.png', alt='')
+        .row.align-items-center.justify-content-center
+            p #[b Las mercancías pasan por diversas manipulaciones antes de ser entregadas al cliente o enviadas al punto de venta, a continuación se muestra el proceso:]
+            ul.lista-ul--color.fa-al
+              li.mb-0 
+                i.fas.fa-shopping-cart
+                span.mb-0 #[em Picking].
+              li.mb-0 
+                i.fas.fa-shopping-cart
+                span.mb-0 Embalaje, etiquetado y pesado.
+              li.mb-0 
+                i.fas.fa-shopping-cart
+                span.mb-0 Consolidación del pedido.
+              li.mb-0 
+                i.fas.fa-shopping-cart
+                span.mb-0 Traslado a la zona de expedición.
+              li.mb-0 
+                i.fas.fa-shopping-cart
+                span.mb-0 Agrupación por destinos.
+              li.mb-0 
+                i.fas.fa-shopping-cart
+                span.mb-0 Verificación.
+
+    p #[b En cuanto a las técnicas de rotulado, se distinguen las siguientes:]
+
+    .row.justify-content-center(data-aos="fade-down") 
+        .col-sm-6.col-xl-4.mb-4.mb-xl-0
+          .tarjeta.tarjeta-slide.arriba(@mouseover="indicadorTarjetaSlide = false")
+            .indicador--hover(v-if="indicadorTarjetaSlide")
+            .tarjeta-slide__contenedor
+              .tarjeta-slide__contenido.p-4.p-xl-5
+                h4.text-center.bgprimario Manual
+                p.text-center Es realizada por el operario encargado de la tarea, quien se encarga de preparar el tipo de rótulo adecuado para cada producto.
+              .tarjeta-slide__img(:style="{'background-image': `url(${require('@/assets/curso/temas/t5/tar1-1.png')})`}")
+        .col-sm-6.col-xl-4.mb-4.mb-xl-0
+          .tarjeta.tarjeta-slide.arriba(@mouseover="indicadorTarjetaSlide = false")
+            .tarjeta-slide__contenedor
+              .tarjeta-slide__contenido.p-4.p-xl-5
+                h4.text-center.bgprimario Mecánica
+                p.text-center Funciona a través de un programa que permite ajustar la velocidad de etiquetado, codificación o rotulación, así como modificar el tamaño del rótulo según lo que se desee indicar en el empaque del producto. 
+              .tarjeta-slide__img(:style="{'background-image': `url(${require('@/assets/curso/temas/t5/tar1-2.png')})`}")
 
 
 </template>
