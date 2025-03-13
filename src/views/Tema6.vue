287c0a9b0247e1cb44c86c63148e3790218d2eeb
Lucenith Pinilla Moreno <167031042+LucenithGitHub@users.noreply.github.com>
2025-03-12 22:46:26 -0500

    Avance 2 03.12

--- src/views/Tema6.vue
+++ src/views/Tema6.vue
@@ -36,8 +36,129 @@
     .etiqueta.sub7.p-2.d-flex.mb-4(data-aos="fade-left")
       h3.mb-0.ps-5.pe-5 Áreas de despacho
 
+    p Las áreas de despacho o entrega de mercancía están determinadas por la naturaleza del producto y la infraestructura de la organización, al igual que las áreas de recepción. Es fundamental que la empresa solicite información precisa sobre el lugar de entrega, ya que en algunas organizaciones existen diferentes puntos de recepción, a continuación se describen las áreas de despacho: 
+
+    .row.align-items-center.mb-4(data-aos="fade-down")
+
+      .col-lg-4.col-md-7.col-9
+        img(src='@/assets/curso/temas/t6/img2.png', alt='')
+
+      .col-lg-8.col-md-12.col-12.px-lg-5.px-4
+            SlyderF.custom(columnas="col-lg-6 col-md-6 col-xl-6")
+              .tarjeta.tarjeta--slyder.b1.p-2.p-lg-4.p-md-3
+                .row.justify-content-center.mb-3
+                  .col-8.my-2
+                    img.img-a.img-t(data-aos="zoom-in-left")(src='@/assets/curso/temas/t6/tar1-1.png' alt='AvatarTop')
+                .bgprimario.p-2.mb-4
+                  h4.text-center.mb-0 Oficina de despacho
+                p.mb-0.text-center(data-aos="fade-right") Es el espacio designado para llevar a cabo las actividades administrativas relacionadas con el despacho de mercancías.     
+              .tarjeta.tarjeta--slyder.b1.p-2.p-lg-4.p-md-3
+                .row.justify-content-center.mb-3
+                  .col-8.my-2
+                    img.img-a.img-t(data-aos="zoom-in-left")(src='@/assets/curso/temas/t6/tar1-2.png' alt='AvatarTop')
+                .bgprimario.p-2.mb-4
+                  h4.text-center.mb-0 Muelles de carga
+                p.mb-0.text-center(data-aos="fade-right") Son las áreas destinadas para el cargue y descarga de los vehículos.   
+              .tarjeta.tarjeta--slyder.b1.p-2.p-lg-4.p-md-3
+                .row.justify-content-center.mb-3
+                  .col-8.my-2
+                    img.img-a.img-t(data-aos="zoom-in-left")(src='@/assets/curso/temas/t6/tar1-3.png' alt='AvatarTop')
+                .bgprimario.p-2.mb-4
+                  h4.text-center.mb-0 Área de alistamiento o preparación (#[em Picking]) 
+                p.mb-0.text-center(data-aos="fade-right") Es el espacio destinado a la preparación de los pedidos que deben ser despachados, considerando las unidades, cajas o productos a granel según corresponda.  
+              .tarjeta.tarjeta--slyder.b1.p-2.p-lg-4.p-md-3
+                .row.justify-content-center.mb-3
+                  .col-8.my-2
+                    img.img-a.img-t(data-aos="zoom-in-left")(src='@/assets/curso/temas/t6/tar1-4.png' alt='AvatarTop')
+                .bgprimario.p-2.mb-4
+                  h4.text-center.mb-0 Área de verificación y salida
+                p.mb-0.text-center(data-aos="fade-right") Es el espacio donde se consolidan las mercancías, se verifican las referencias y cantidades, y, una vez confirmados, se procede al empaque, utilizando materiales adecuados según la naturaleza de los productos y el medio de transporte.
 
 
+    .etiqueta.sub8.p-2.d-flex.mb-4(data-aos="fade-left")
+      h3.mb-0.ps-5.pe-5 Procedimiento de despacho de objetos
+
+    p.mb-4 Al igual que en el proceso de recepción de mercancía, el procedimiento de despacho está determinado por la naturaleza y el tipo de carga. En términos generales, el proceso sigue los siguientes pasos:
+
+    .row.align-items-center.justify-content-center.fondotema61.mb-4
+      .col-sm-12.col-md-12.col-lg-10
+        PasosA.color-acento-contenido.mb-0(tipo="l")
+          .row.align-items-center.justify-content-center
+            .col-lg
+              h3 Recepción del pedido
+              p.mb-0 Se verifica que el documento contenga toda la información necesaria para llevar a cabo el despacho de manera correcta.
+            .col-auto
+              figure(data-aos="zoom-in")
+                img.d-none.d-lg-block(src='@/assets/curso/temas/t6/ps1-1.png', alt='')
+          .row.align-items-center.justify-content-center
+            .col-auto
+              figure(data-aos="zoom-in")
+                img.d-none.d-lg-block(src='@/assets/curso/temas/t6/ps1-2.png', alt='')
+            .col-lg
+              h3 Verificación en el sistema de inventarios
+              p Se confirma que las referencias y cantidades estén disponibles en el inventario para ser entregadas.
+          .row.align-items-center.justify-content-center
+            .col-lg
+              h3 Programación del despacho
+              p.mb-0 Se organiza el despacho según la ciudad de destino y el medio de transporte elegido, asegurando la correcta planificación de los tiempos y rutas.
+            .col-auto
+              figure(data-aos="zoom-in")
+                img.d-none.d-lg-block(src='@/assets/curso/temas/t6/ps1-3.png', alt='')
+          .row.align-items-center.justify-content-center
+            .col-auto
+              figure(data-aos="zoom-in")
+                img.d-none.d-lg-block(src='@/assets/curso/temas/t6/ps1-4.png', alt='')
+            .col-lg
+              h3 Preparación del pedido (#[em Picking])
+              p Se realiza la preparación de los pedidos solicitados, unificando los distintos pedidos si es necesario, y asegurando la correcta documentación, referencias y cantidades solicitadas.
+          .row.align-items-center.justify-content-center
+            .col-lg
+              h3 Trasladar los productos
+              p.mb-0 A la zona de alistamiento para su despacho o zona de #[em picking].
+            .col-auto
+              figure(data-aos="zoom-in")
+                img.d-none.d-lg-block(src='@/assets/curso/temas/t6/ps1-5.png', alt='')
+          .row.align-items-center.justify-content-center
+            .col-auto
+              figure(data-aos="zoom-in")
+                img.d-none.d-lg-block(src='@/assets/curso/temas/t6/ps1-6.png', alt='')
+            .col-lg
+              h3 Realizar el acondicionamiento final de los pedidos
+              p Lo cual incluye una nueva verificación de los mismos.
+          .row.align-items-center.justify-content-center
+            .col-lg
+              h3 Coordinar el envío de la mercancía
+              p.mb-0 Ya sea mediante transporte propio, transporte del cliente o una empresa transportadora.
+            .col-auto
+              figure(data-aos="zoom-in")
+                img.d-none.d-lg-block(src='@/assets/curso/temas/t6/ps1-7.png', alt='')
+          .row.align-items-center.justify-content-center
+            .col-auto
+              figure(data-aos="zoom-in")
+                img.d-none.d-lg-block(src='@/assets/curso/temas/t6/ps1-8.png', alt='')
+            .col-lg
+              h3 Identificar el pedido del cliente
+              p Mediante un rótulo y asegurarse de que todos los documentos necesarios para la entrega de la mercancía estén listos.
+          .row.align-items-center.justify-content-center
+            .col-lg
+              h3 Al recibir el vehículo para el cargue
+              p.mb-0 Verificar que cumpla con las condiciones necesarias, de acuerdo con las especificaciones para el cargue y traslado de los productos.
+            .col-auto
+              figure(data-aos="zoom-in")
+                img.d-none.d-lg-block(src='@/assets/curso/temas/t6/ps1-9.png', alt='')
+          .row.align-items-center.justify-content-center
+            .col-auto
+              figure(data-aos="zoom-in")
+                img.d-none.d-lg-block(src='@/assets/curso/temas/t6/ps1-10.png', alt='')
+            .col-lg
+              h3 Realizar el cargue de los productos en el vehículo de forma adecuada. 
+          .row.align-items-center.justify-content-center
+            .col-lg
+              h3 Mantener una comunicación constante 
+              p.mb-0 Con el transportista para garantizar la correcta entrega del producto. Este proceso se denomina trazabilidad.
+            .col-auto
+              figure(data-aos="zoom-in")
+                img.d-none.d-lg-block(src='@/assets/curso/temas/t6/ps1-11.png', alt='')
 
 </template>
 
--- src/views/Tema7.vue
+++ src/views/Tema7.vue
@@ -5,7 +5,67 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 7
-      h1 Tema 7
+      h1 Preparación y organización de áreas para recibo y despacho
+
+    .row.align-items-center.justify-content-center.mb-4
+      .col-2.col-md-2.col-lg-1
+        img(src='@/assets/curso/temas/t7/ico1.png', alt='')
+      .col-sm-12.col-md-12.col-lg-11
+        p Es fundamental considerar que el proceso de recepción y despacho varía según el tipo de producto y su empaque. Dependiendo de las características del empaque, se requiere el uso de equipos específicos para la movilización, tales como estibas o paletas, así como tanques para almacenar productos a granel (líquidos o sólidos). Además, es importante destacar que las empresas diseñan sus espacios logísticos según sus necesidades particulares.
+
+    .etiqueta.sub9.p-2.d-flex.mb-4(data-aos="fade-left")
+      h3.mb-0.ps-5.pe-5 Características de las áreas de recibido
+
+    p.mb-4 Existen múltiples características a tener en cuenta, ya que, como se ha mencionado a lo largo del material, todo depende de la naturaleza del producto y de la infraestructura disponible. A continuación, se mencionan algunos aspectos clave a considerar para diferentes tipos de productos.
+    
+    .row.mb-3
+      .col-lg-6.col-sm-12.col-md-12
+        h3.p-2.bggris Aspectos claves a considerar en el área de recibo de mercancías
+
+    .tarjeta--container.row.mb-5
+        .col-md.tarjeta.color-secundario.p-4.p-md-4.p-lg-5(data-aos="fade-right")
+          .row.justify-content-center.mb-4
+            .col-12
+              .row.align-items-end
+                .col-lg-3.mb-4
+                  img.img-a.img-t(src='@/assets/curso/temas/t7/tar1-1.png', alt='Imagen decorativa.')
+                .col-lg-7
+                  h2.mb-4.bgnegro.text-white.text-center Despeje del área de recibo
+              p.mb-0 Es fundamental mantener el área despejada para facilitar el flujo constante de mercancía, evitando de esta manera represamientos que afecten la eficiencia de la operación.
+        .col-md.tarjeta.color-primario.p-4.p-md-4.p-lg-5(data-aos="fade-left")
+          .row.justify-content-center.mb-1
+            .col-12
+              .row.align-items-end
+                .col-lg-3.mb-4
+                  img.img-a.img-t(src='@/assets/curso/temas/t7/tar1-2.png', alt='Imagen decorativa.')
+                .col-lg-7
+                  h2.mb-4.bgnegro.text-white.text-center Espacio adecuado 
+              p.mb-0 El tamaño del espacio de recibo debe estar determinado por el volumen de mercancía y el tiempo de descarga, el cual debe ser el más breve posible para reducir costos operativos.
+
+    .etiqueta.sub10.p-2.d-flex.mb-4(data-aos="fade-left")
+      h3.mb-0.ps-5.pe-5 Recibo de alimentos
+ 
+    .row.justify-content-center.align-items-center.mb-4
+      .col-6.col-md-6.col-lg-3
+        img(src='@/assets/curso/temas/t7/img1.png', alt='')
+      .col-sm-12.col-md-12.col-lg-9
+        AcordionA(tipo="a" clase-tarjeta="tarjeta tarjeta--azul")
+          .row.col-xl-11.m-auto(titulo="Control de la cadena de frío")
+            .row.justify-content-center.align-items-center(data-aos="fade-right")
+              .col-lg-12
+                p.mb-0 Algunos productos, especialmente los alimentos, requieren mantener una temperatura específica durante todo el proceso de recepción y almacenamiento para evitar su descomposición y la proliferación de bacterias, lo que podría representar un riesgo para la salud del consumidor. Es esencial contar con un área definida y adecuada para este tipo de mercancía.
+          .row.col-xl-11.m-auto(titulo="Control de materias primas")
+            .row.justify-content-center.align-items-center(data-aos="fade-right")
+              .col-lg-12
+                p.mb-0 Es necesario contar con un área de básculas calibradas para garantizar la precisión en el pesaje de las materias primas y minimizar pérdidas. Además, se deben clasificar correctamente estas materias primas, y algunas empresas demarcan sus áreas de recibo conforme a especificaciones específicas.
+          .row.col-xl-11.m-auto(titulo="Requisitos de almacenamiento")
+            .row.justify-content-center.align-items-center(data-aos="fade-right")
+              .col-lg-12
+                p.mb-0 Algunas materias primas requieren condiciones especiales de almacenamiento, como refrigeración o espacios con control de humedad. Es crucial identificar y definir estos espacios según las necesidades específicas de cada producto.
+
+    .etiqueta.sub11.p-2.d-flex.mb-4(data-aos="fade-left")
+      h3.mb-0.ps-5.pe-5 Variables que influyen en el espacio de recibo
+
 
 
 </template>
